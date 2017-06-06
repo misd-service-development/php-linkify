fed3bae99145a8b81e399c5a4ca23cfbd036c77c
Danny <gqwebsites@gmail.com>
2017-06-06 13:47:50 -0400

    changed linkify menation regex

--- src/Misd/Linkify/Linkify.php
+++ src/Misd/Linkify/Linkify.php
@@ -234,7 +234,7 @@ class Linkify implements LinkifyInterface
 
     protected function linkifyTwitter($text, $options = array('attr' => ''))
     {
-        $pattern = '/\B@[^\B]([^.\s]+)/';
+        $pattern = '/\B@[^]([^.\s]+)/';
         //$pattern  = '/@([\w]+)([[^\s])/i'; 
         ///$pattern  = '/@([\w]+)([^\s]+)/i';
 
